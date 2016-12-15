b5be0d802c73cbca8ae7846f838ebe8e0cd9dad8
Stefano Peruzzi <s.peruzzi@gmail.com>
2016-12-15 17:36:32 +0100

    buyCoffees senza var

--- src/main/scala/progfun/App.scala
+++ src/main/scala/progfun/App.scala
@@ -26,20 +26,13 @@ object App {
   }
 
   def buyCoffees(cc: CartaCredito, n: Int): List[(Coffee, Payment)] = {
-    // con var result e' ri-assegnabile
-    var result = List[(Coffee, Payment)]()
-
-    for (i <- Range(0, n))
-      result = buyCoffee(cc) :: result
-
-    result
+    Range(0, n).map((i: Int) => buyCoffee(cc)).toList
   }
 
   /**
     * Program entry point.
     */
   def main(args: Array[String]) {
-  /*
     val cc = new CartaCredito
     val coffeePayment = buyCoffee(cc)
     println("Ho preso un " + coffeePayment._1)
@@ -48,12 +41,6 @@ object App {
     val coffees = buyCoffees(cc, 10)
     println(s"comprati ${coffees.length} caffe', da pagare!")
     coffees.map(_._2.pay())
-    */
-
-    println(TestPure.areEqual())
-    println(TestPure2.areEqual())
-    println(TestPure3.areEqual())
-    println(TestPure4.areEqual())
   }
 
 }
